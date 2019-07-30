2fd0427ba7f15a4ca8405fa93d8b2c0fa2e77d64
piesoftsol <35703117+piesoftsol@users.noreply.github.com>
2019-07-30 23:05:26 +0530

    Add files via upload

---
+++ src/main/java/com/kj/oneservice/common/integration/util/SwaggerConstants.java
@@ -0,0 +1,15 @@
+package com.kj.oneservice.common.integration.util;
+
+/**
+ * Class to hold all Swagger related constants
+ * 
+ * @author ArchanaK
+ */
+public class SwaggerConstants {
+
+	public static final String EXP_REQUEST_ID = "fc612640-2d12-44e6-9071-9e6a91085cfa";
+	public static final String EXP_REQUEST_INTIME_ID = "2019-06-27 16:48:30";
+	public static final String EXP_REQUEST_OUTTIME_ID = "2019-06-27 16:48:12";
+	public static final String EXP_PROCESS_TIME_ID = "30";
+	public static final String EXP_RESPONSE_CODE = "200";
+}
